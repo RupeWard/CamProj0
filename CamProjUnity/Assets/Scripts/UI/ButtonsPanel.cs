3277f6c4403f8d625f607f0e8be08ee4f5a30b1b
RupeWard <rupe@rupertward.co.uk>
2015-11-08 01:18:27 +0000

    Button hiding

---
+++ CamProjUnity/Assets/Scripts/UI/ButtonsPanel.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonsPanel : MonoBehaviour
+{
+	public GameObject outButton;
+
+	private Vector3 outPosition_;
+	private Vector3 inPosition_;
+
+	private RectTransform rectTransform_;
+
+	public void OnOutButtonClicked()
+	{
+		rectTransform_.anchoredPosition = outPosition_;
+		outButton.gameObject.SetActive( false );
+	}
+
+	public void OnInButtonClicked()
+	{
+		rectTransform_.anchoredPosition = inPosition_;
+		outButton.gameObject.SetActive( true );
+	}
+
+	public void OnQuitButtonClicked()
+	{
+#if UNITY_EDITOR
+		Debug.LogWarning( "No Quit in Editor" );
+#else
+		if (Application.platform == RuntimePlatform.Android)
+		{
+			Application.Quit( );
+		}
+		else
+		{
+			Debug.LogWarning( "No Quit in "+Application.platform  );
+		}
+#endif
+	}
+
+	private void Awake()
+	{
+		rectTransform_ = GetComponent<RectTransform>( );
+		outPosition_ = rectTransform_.anchoredPosition;
+		inPosition_ = outPosition_;
+		inPosition_.x = inPosition_.x + GetComponent<RectTransform>( ).GetWidth( );
+		outButton.gameObject.SetActive( false );
+	}
+}
